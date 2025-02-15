d5bd2b983f29e0a17d809915bee303cb2a5b8883
Yan Carlos <contact@yancarlos.tech>
2025-02-15 09:35:26 -0300

    Order Item Service finalized.

---
+++ src/main/java/com/dev/yank/ecommerce/services/OrderItemService.java
@@ -0,0 +1,63 @@
+package com.dev.yank.ecommerce.services;
+
+import com.dev.yank.ecommerce.dto.OrderItemDTO;
+import com.dev.yank.ecommerce.exception.OrderItemNotFoundException;
+import com.dev.yank.ecommerce.mapper.OrderItemMapper;
+import com.dev.yank.ecommerce.model.OrderItem;
+import com.dev.yank.ecommerce.repository.OrderItemRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderItemService {
+
+    // Injection
+    private final OrderItemRepository orderItemRepository;
+    private final OrderItemMapper orderItemMapper;
+
+    public OrderItemService(OrderItemRepository orderItemRepository, OrderItemMapper orderItemMapper) {
+        this.orderItemRepository = orderItemRepository;
+        this.orderItemMapper = orderItemMapper;
+    }
+
+    // Methods
+    public List<OrderItemDTO> getOrderItems() {
+        return orderItemRepository.findAll()
+                .stream()
+                .map(orderItemMapper::toDTO).toList();
+    }
+
+    public OrderItemDTO getOrderItemById(Long id) {
+        OrderItem orderItem = orderItemRepository.findById(id)
+                .orElseThrow(() -> new OrderItemNotFoundException("Order Item not found with ID: " + id));
+        return orderItemMapper.toDTO(orderItem);
+    }
+
+    @Transactional
+    public OrderItemDTO createOrderItem(OrderItemDTO newOrderItem) {
+        OrderItem orderItem = orderItemMapper.toEntity(newOrderItem);
+        OrderItem savedOrderItem = orderItemRepository.save(orderItem);
+        return orderItemMapper.toDTO(savedOrderItem);
+    }
+
+    @Transactional
+    public OrderItemDTO updateOrderItem(Long id, OrderItemDTO updateOrderItem) {
+        OrderItem existingOrderItem = orderItemRepository.findById(id)
+                .orElseThrow(() -> new OrderItemNotFoundException("Order Item not found with ID: " + id));
+
+        existingOrderItem.setPrice(updateOrderItem.price());
+        existingOrderItem.setQuantity(updateOrderItem.quantity());
+
+        OrderItem updatedOrderItem = orderItemRepository.save(existingOrderItem);
+        return orderItemMapper.toDTO(existingOrderItem);
+    }
+
+    @Transactional
+    public void deleteOrderItem(Long id) {
+        if (!orderItemRepository.existsById(id)) {
+            throw new OrderItemNotFoundException("Order Item not found with ID: " + id);
+        }
+    }
+}
