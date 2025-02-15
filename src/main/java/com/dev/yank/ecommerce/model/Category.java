9ddeae8f7368709f78409ac7e0a9581712bac99a
Yan Carlos <contact@yancarlos.tech>
2025-02-15 16:18:00 -0300

    Update Model

--- src/main/java/com/dev/yank/ecommerce/model/Category.java
+++ src/main/java/com/dev/yank/ecommerce/model/Category.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/java/com/dev/yank/ecommerce/model/Review.java
+++ src/main/java/com/dev/yank/ecommerce/model/Review.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Review {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private int rating;
