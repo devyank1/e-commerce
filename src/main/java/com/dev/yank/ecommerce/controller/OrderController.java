f2bd1d5e841ecbd1559cabf9244a3ffaf19254b7
Yan Carlos <contact@yancarlos.tech>
2025-02-16 13:38:12 -0300

    Order Controller finalized.

---
+++ src/main/java/com/dev/yank/ecommerce/controller/OrderController.java
@@ -0,0 +1,48 @@
+package com.dev.yank.ecommerce.controller;
+
+import com.dev.yank.ecommerce.dto.OrderDTO;
+import com.dev.yank.ecommerce.services.OrderService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<OrderDTO>> getOrders(Pageable pageable) {
+        Page<OrderDTO> orders = orderService.getOrders(pageable);
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long id) {
+        OrderDTO order = orderService.getOrderById(id);
+        return ResponseEntity.ok(order);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO newOrder) {
+        OrderDTO order = orderService.createOrder(newOrder);
+        return ResponseEntity.ok(order);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long id, @RequestBody OrderDTO updateOrder) {
+        OrderDTO order = orderService.updateOrder(id, updateOrder);
+        return ResponseEntity.ok(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+}
