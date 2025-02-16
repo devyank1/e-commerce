445c0dd7c7e1488fc5fcdfcb515b6f1d624c5e83
Yan Carlos <contact@yancarlos.tech>
2025-02-16 14:28:02 -0300

    Review Controller finalized.

---
+++ src/main/java/com/dev/yank/ecommerce/controller/ReviewController.java
@@ -0,0 +1,49 @@
+package com.dev.yank.ecommerce.controller;
+
+import com.dev.yank.ecommerce.dto.ReviewDTO;
+import com.dev.yank.ecommerce.services.ReviewService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ReviewDTO>> getReviews(Pageable pageable) {
+        Page<ReviewDTO> reviews = reviewService.getReviews(pageable);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReviewDTO> getReviewById(@PathVariable Long id) {
+        ReviewDTO review = reviewService.getReviewById(id);
+        return ResponseEntity.ok(review);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReviewDTO> createReview(@RequestBody ReviewDTO newReview) {
+        ReviewDTO review = reviewService.createReview(newReview);
+        return ResponseEntity.ok(review);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewDTO> updateReview(@PathVariable Long id, @RequestBody ReviewDTO updateReview) {
+        ReviewDTO review = reviewService.updateReview(id, updateReview);
+        return ResponseEntity.ok(review);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+
+}
