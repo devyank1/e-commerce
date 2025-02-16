2aaa15a5fe1d13d5b6d7cb9718a5a51d96d56248
Yan Carlos <contact@yancarlos.tech>
2025-02-16 14:15:13 -0300

    Product Controller finalized.

---
+++ src/main/java/com/dev/yank/ecommerce/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.dev.yank.ecommerce.controller;
+
+import com.dev.yank.ecommerce.dto.ProductDTO;
+import com.dev.yank.ecommerce.services.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> getProducts(Pageable pageable) {
+        Page<ProductDTO> products = productService.getProducts(pageable);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        ProductDTO product = productService.getProductsById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO newProduct) {
+        ProductDTO product = productService.createProduct(newProduct);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody ProductDTO updateProduct) {
+        ProductDTO product = productService.updateProduct(id, updateProduct);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+}
