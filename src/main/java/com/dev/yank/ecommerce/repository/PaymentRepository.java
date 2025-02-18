7a213c4656a16831c4ebc70ede7d484400a7df6c
Yan Carlos <contact@yancarlos.tech>
2025-02-18 14:38:36 -0300

    Added 'existsByOrderId' and 'findByTransactionId'.

--- src/main/java/com/dev/yank/ecommerce/repository/PaymentRepository.java
+++ src/main/java/com/dev/yank/ecommerce/repository/PaymentRepository.java
@@ -4,6 +4,11 @@ import com.dev.yank.ecommerce.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    boolean existsByOrderId(Long orderId);
+
+    Optional<Payment> findByTransactionId(String transactionId);
 }
