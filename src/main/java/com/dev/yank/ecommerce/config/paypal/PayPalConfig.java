0d527d35f717a3d3135152094426efbbf84bf2a8
Yan Carlos <contact@yancarlos.tech>
2025-02-18 17:28:53 -0300

    Update PayPal Config.

--- src/main/java/com/dev/yank/ecommerce/config/paypal/PayPalConfig.java
+++ src/main/java/com/dev/yank/ecommerce/config/paypal/PayPalConfig.java
@@ -1,36 +1,23 @@
 package com.dev.yank.ecommerce.config.paypal;
 
-import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class PayPalConfig {
 
-    @Value("${server.paypal.client-id}")
+    @Value("${paypal.client-id}")
     private String clientId;
 
-    @Value("${server.paypal.secret}")
-    private String clientSecret;
+    @Value("${paypal.secret}")
+    private String secret;
 
-    @Value("${server.paypal.mode}")
-    private String mode;
-
-    @Bean
-    public APIContext apiContext()throws PayPalRESTException {
-        APIContext apiContext = new APIContext(clientId, clientSecret, mode);
-        apiContext.setConfigurationMap(paypalSdkConfig());
-        return apiContext;
+    public String getClientId() {
+        return clientId;
     }
 
-    private Map<String, String> paypalSdkConfig() {
-        Map<String, String> configMap = new HashMap<>();
-        configMap.put("mode", mode);
-        return configMap;
+    public String getSecret() {
+        return secret;
     }
 }
