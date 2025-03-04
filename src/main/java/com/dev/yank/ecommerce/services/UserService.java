37d02134ede12ee9088cec28283407aead744224
Yan Carlos <contact@yancarlos.tech>
2025-03-04 14:02:09 -0300

    User Service updated

--- src/main/java/com/dev/yank/ecommerce/services/UserService.java
+++ src/main/java/com/dev/yank/ecommerce/services/UserService.java
@@ -4,10 +4,12 @@ import com.dev.yank.ecommerce.dto.UserDTO;
 import com.dev.yank.ecommerce.exception.UserNotFoundException;
 import com.dev.yank.ecommerce.mapper.UserMapper;
 import com.dev.yank.ecommerce.model.User;
+import com.dev.yank.ecommerce.model.enums.Role;
 import com.dev.yank.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +20,12 @@ public class UserService {
     // Injection
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // Methods
@@ -62,5 +66,18 @@ public class UserService {
         if (!userRepository.existsById(id)) {
             throw new UserNotFoundException("User not found with ID: " + id);
         }
+    }
+
+    public void registerUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.email())) {
+            throw new RuntimeException("Email is already registered.");
+        }
+
+        User user = new User();
+        user.setEmail(userDTO.email());
+        user.setPassword(passwordEncoder.encode(userDTO.password()));
+        user.setRole(Role.CUSTOMER);
+
+        userRepository.save(user);
     }
 }
