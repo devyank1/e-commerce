3c2104c85dd6ee3dba80aa891cbd33f605af017b
Yan Carlos <contact@yancarlos.tech>
2025-03-04 14:04:28 -0300

    Authentication Controller implemented.

---
+++ src/main/java/com/dev/yank/ecommerce/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.dev.yank.ecommerce.controller;
+
+import com.dev.yank.ecommerce.dto.UserDTO;
+import com.dev.yank.ecommerce.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
+        userService.registerUser(userDTO);
+        return ResponseEntity.ok("User registered successfully.");
+    }
+}
