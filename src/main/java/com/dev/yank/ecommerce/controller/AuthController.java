08412dac57aec067ace947576821d02f84f2894b
Yan Carlos <contact@yancarlos.tech>
2025-04-15 09:21:16 -0300

    Added /register & /login

--- src/main/java/com/dev/yank/ecommerce/controller/AuthController.java
+++ src/main/java/com/dev/yank/ecommerce/controller/AuthController.java
@@ -1,26 +1,33 @@
 package com.dev.yank.ecommerce.controller;
 
 import com.dev.yank.ecommerce.dto.UserDTO;
+import com.dev.yank.ecommerce.services.AuthService;
 import com.dev.yank.ecommerce.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
         userService.registerUser(userDTO);
         return ResponseEntity.ok("User registered successfully.");
+    }
+
+    @PostMapping("/login")
+    public Map<String, Object> login(@RequestParam String username, @RequestParam String password) {
+        return authService.login(username, password);
     }
 }
